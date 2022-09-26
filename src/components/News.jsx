d1d0772ff050c1d9ed35f1cd1f71951816dbc0cb
Noah Weinberg <noahwrg@gmail.com>
2022-09-26 16:25:53 -0400

    updates to components and redux setup

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import {Select, Typography, Row, Col, Avatar, Card} from 'antd'
+import moment from 'moment';
 
-const News = () => {
+import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
+
+const {Text, Title} = Typography;
+const {Option} = Select;
+
+const News = ({simplified}) => {
+  const {data: cryptoNews} = useGetCryptoNewsQuery({newsCategory: 'Crypto', count: simplified ? 6 : 12});
+  if(!cryptoNews?.value) return "Loading...";
+
   return (
-    <div>News</div>
+    <Row gutter={[24, 24]}>
+        {cryptoNews.value.map((article, i) => (
+            <Col xs={24} sm={12} lg={8} key={i}>
+              <Card hoverable className="news-card">
+                <a href={article.url} target="_blank" rel="noreferrer">
+                  <div className="news-image-container">
+                    <Title className="news-title" level={4}>{article.name}</Title>
+                  </div>
+                </a>
+              </Card>
+            </Col>
+        ))}
+    </Row>
   )
 }
 
