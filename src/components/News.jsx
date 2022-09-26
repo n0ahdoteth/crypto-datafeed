e1f4fc4c336bed54a2a98d86631d207d45750719
Noah Weinberg <noahwrg@gmail.com>
2022-09-26 16:25:53 -0400

    updated news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Select, Typography, Row, Col, Avatar, Card} from 'antd'
-import moment from 'moment';
+
 
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 
